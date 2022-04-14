b4a43cb876d989872ee7cdb73aa8c808908d398e
Vinicio Huertas <vinicio.huertas@registrocivil.gob.ec>
2022-04-13 19:59:05 -0500

    FEAT: Insert user, role

--- employeesvaccination/src/main/resources/scripts/data.sql
+++ employeesvaccination/src/main/resources/scripts/data.sql
@@ -17,3 +17,17 @@ INSERT INTO employeevaccine.catalogue_detail (abbreviation_detail, name_detail, 
 VALUES ('pfizer', 'Pfizer', 'Pfizer vaccine', true, 1);
 INSERT INTO employeevaccine.catalogue_detail (abbreviation_detail, name_detail, description_detail, status_detail, catalogue_id_catalogue) 
 VALUES ('jhonson', 'Jhonson&Jhonson', 'Jhonson&Jhonson vaccine', true, 1);
+
+
+-- DB: postgres
+-- SCHEMA: employeevaccine
+-- TABLE: users
+-- DESC: insert ADMIN user
+INSERT INTO employeevaccine.users (password, username, enabled) VALUES ('$2a$10$w4/PY713tJmT/.XU7DCizeJE76oSCgSwkNFIzKFUONrryxeKihMFW', 'admin', true);
+
+
+-- DB: postgres
+-- SCHEMA: employeevaccine
+-- TABLE: catalogue_detail
+-- DESC: insert authorities
+INSERT INTO employeevaccine.authorities (authority, users_id) VALUES ('ADMIN', 1);
